2d49eddd69bcb4d20f2acbc04eda89749226f0d1
almahmuddev <devalmahmud@gmail.com>
2026-05-29 16:30:49 +0600

    removed typescript from codes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Images from external sources can be added here later
-  images: {
-    domains: [],
-  },
-}
-
-export default nextConfig
--- public/assets/README.txt
+++
@@ -1,1 +0,0 @@
-Copy the original template assets/ folder contents here (css, js, images, fonts)
