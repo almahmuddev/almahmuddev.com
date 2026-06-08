4ae3c7c595253e4020f4fd1f4f54fdf3d7e1bee9
almahmuddev <devalmahmud@gmail.com>
2026-06-08 12:24:30 +0600

    add expertise section with radial progress indicators

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Portfolio from '@/components/Portfolio'
+import Expertise from '@/components/Expertise'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Portfolio />
+      <Expertise />
     </main>
   )
 }
